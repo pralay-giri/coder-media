7b2fc0c848cad7f7e2e37d238a5debfdbf345c42
wasimmiddya <wasimmiddya21@gmail.com>
2024-02-19 01:03:51 +0530

    common server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,11 @@
 import express from 'express'
 
+const app = express()
 
+app.get('/', (req, res, next) => {
+    return res.status(200).send("<h2 style='color: green'>Set Up Successfull!</h2>");
+})
 
+app.listen(8000, () => {
+    console.log('app is listing');
+})
